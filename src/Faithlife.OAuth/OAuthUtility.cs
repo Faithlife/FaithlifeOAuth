96505cca6810f2f04904b656b2c8e21279db675c
Jason Tucker <jason.tucker@faithlife.com>
2021-07-30 11:36:45 -0400

    Reducing scope of unneeded Sha256 methods. Use suggested changes from https://git.faithlife.dev/Logos/Utility/pull/297

--- src/Faithlife.OAuth/OAuthUtility.cs
+++ src/Faithlife.OAuth/OAuthUtility.cs
@@ -55,47 +55,6 @@ namespace Faithlife.OAuth
 		}
 
 		/// <summary>
-		/// Create uri to use with an OAuth authentication header created using the HMACSHA256 signature method.
-		/// </summary>
-		public static Uri CreateHmacSha256Uri(Uri uri, string httpMethod, string consumerToken, string consumerSecret, string? callback = null)
-		{
-			List<KeyValuePair<string, string>>? additionalParameters = null;
-			if (callback != null)
-				additionalParameters = new List<KeyValuePair<string, string>>() { NewKeyValuePair(OAuthConstants.Callback, callback) };
-			var signatureBase = CreateSignatureBase(uri, httpMethod, GetHmacSha256Parameters(consumerToken, s_nonceCreator, s_systemTime, additionalParameters), out var newUri);
-			return new Uri("{0}&{1}={2}".FormatInvariant(newUri, OAuthConstants.Signature, CreateHmacSha256Signature(signatureBase, consumerSecret, null)));
-		}
-
-		/// <summary>
-		/// Create uri to use with an OAuth authentication header created using the HMACSHA256 signature method.
-		/// </summary>
-		public static Uri CreateHmacSha256Uri(Uri uri, string httpMethod, string consumerToken, string consumerSecret, string accessToken, string accessSecret, string? verifier = null)
-		{
-			var additionalParameters = new List<KeyValuePair<string, string>> { NewKeyValuePair(OAuthConstants.Token, accessToken) };
-			if (verifier != null)
-				additionalParameters.Add(NewKeyValuePair(OAuthConstants.Verifier, verifier));
-			var signatureBase = CreateSignatureBase(uri, httpMethod, GetHmacSha256Parameters(consumerToken, s_nonceCreator, s_systemTime, additionalParameters), out var newUri);
-			return new Uri("{0}&{1}={2}".FormatInvariant(newUri, OAuthConstants.Signature, CreateHmacSha256Signature(signatureBase, consumerSecret, accessSecret)));
-		}
-
-		/// <summary>
-		/// Create OAuth signature using the HMACSHA256 signature method.
-		/// </summary>
-		public static string CreateHmacSha256Signature(Uri uri, string httpMethod, string consumerToken, string consumerSecret, string nonce, string timeStamp, IEnumerable<KeyValuePair<string, string>>? formParameters = null) =>
-			CreateHmacSha256Signature(uri, httpMethod, consumerSecret, null, GetHmacSha256Parameters(consumerToken, nonce, timeStamp, formParameters));
-
-		/// <summary>
-		/// Create OAuth authentication header using the HMACSHA256 signature method. Accepts arbitrary number of form parameters.
-		/// </summary>
-		public static string CreateHmacSha256AuthorizationHeaderValue(Uri uri, string httpMethod, string consumerToken, string consumerSecret, INonceCreator? nonceCreator = null, ISystemTime? systemTime = null, string? temporaryToken = null, string? temporarySecret = null, IEnumerable<KeyValuePair<string, string>>? formParameters = null)
-		{
-			var additionalParameters = formParameters.EmptyIfNull().ToList();
-			if (temporaryToken != null)
-				additionalParameters.Add(NewKeyValuePair(OAuthConstants.Token, temporaryToken));
-			return CreateHmacSha256AuthorizationHeaderValue(uri, httpMethod, consumerSecret, temporarySecret, GetHmacSha256Parameters(consumerToken, nonceCreator ?? s_nonceCreator, systemTime ?? s_systemTime, additionalParameters));
-		}
-
-		/// <summary>
 		/// Create uri to use with an OAuth authentication header created using the HMACSHA1 signature method.
 		/// </summary>
 		public static Uri CreateHmacSha1Uri(Uri uri, string httpMethod, string consumerToken, string consumerSecret)
@@ -155,7 +114,6 @@ namespace Faithlife.OAuth
 		public static string CreateHmacSha1Signature(Uri uri, string httpMethod, string consumerToken, string consumerSecret, string temporaryToken, string temporarySecret, string verifier, string nonce, string timeStamp) =>
 			CreateHmacSha1Signature(uri, httpMethod, consumerSecret, temporarySecret, GetHmacSha1Parameters(consumerToken, nonce, timeStamp, new Dictionary<string, string> { { OAuthConstants.Token, temporaryToken }, { OAuthConstants.Verifier, verifier } }));
 
-
 		/// <summary>
 		/// Create OAuth authentication header using the HMACSHA1 signature method.
 		/// </summary>
@@ -183,23 +141,11 @@ namespace Faithlife.OAuth
 		private static string CreateHmacSha1Signature(Uri uri, string httpMethod, string consumerSecret, string? tokenSecret, ICollection<KeyValuePair<string, string>> parameters) =>
 			CreateHmacSha1Signature(CreateSignatureBase(uri, httpMethod, parameters, out _), consumerSecret, tokenSecret);
 
-		private static string CreateHmacSha256Signature(Uri uri, string httpMethod, string consumerSecret, string? tokenSecret, ICollection<KeyValuePair<string, string>> parameters) =>
-			CreateHmacSha256Signature(CreateSignatureBase(uri, httpMethod, parameters, out string _), consumerSecret, tokenSecret);
-
 		internal static string CreateHmacSha1Signature(string signatureBase, string consumerSecret, string? tokenSecret)
 		{
 			using var hmacsha1 = new HMACSHA1(Encoding.UTF8.GetBytes(CreatePlainTextSignature(consumerSecret, tokenSecret)));
 			return Convert.ToBase64String(hmacsha1.ComputeHash(Encoding.UTF8.GetBytes(signatureBase)));
 		}
-
-		internal static string CreateHmacSha256Signature(string signatureBase, string consumerSecret, string? tokenSecret)
-		{
-			using var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(CreatePlainTextSignature(consumerSecret, tokenSecret)));
-			return Convert.ToBase64String(hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(signatureBase)));
-		}
-
-		private static string CreateHmacSha256AuthorizationHeaderValue(Uri uri, string httpMethod, string consumerSecret, string? tokenSecret, ICollection<KeyValuePair<string, string>> parameters) =>
-			CreateHmacShaAuthorizationHeaderValue(uri, httpMethod, consumerSecret, tokenSecret, parameters, CreateHmacSha256Signature(CreateSignatureBase(uri, httpMethod, parameters, out var newUIUri), consumerSecret, tokenSecret));
 
 		private static string CreateHmacSha1AuthorizationHeaderValue(Uri uri, string httpMethod, string consumerSecret, string? tokenSecret, ICollection<KeyValuePair<string, string>> parameters) =>
 			CreateHmacShaAuthorizationHeaderValue(uri, httpMethod, consumerSecret, tokenSecret, parameters, CreateHmacSha1Signature(CreateSignatureBase(uri, httpMethod, parameters, out var newUIUri), consumerSecret, tokenSecret));
@@ -216,24 +162,48 @@ namespace Faithlife.OAuth
 				.Join(",");
 		}
 
+		/// <summary>
+		/// Create OAuth authentication header using the HMACSHA256 signature method.
+		/// </summary>
+		public static string CreateHmacSha256AuthorizationHeaderValue(Uri uri, string httpMethod, string consumerToken, string consumerSecret, string accessToken, string accessSecret, INonceCreator? nonceCreator = null, ISystemTime? systemTime = null) =>
+			CreateHmacSha256AuthorizationHeaderValue(uri, httpMethod, consumerSecret, accessSecret, GetHmacSha256Parameters(consumerToken, nonceCreator ?? s_nonceCreator, systemTime ?? s_systemTime, new List<KeyValuePair<string, string>> { NewKeyValuePair(OAuthConstants.Token, accessToken) }));
+
+		internal static string CreateHmacSha256Signature(string signatureBase, string consumerSecret, string tokenSecret)
+		{
+			using var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(CreatePlainTextSignature(consumerSecret, tokenSecret)));
+			return Convert.ToBase64String(hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(signatureBase)));
+		}
+
+		private static string CreateHmacSha256AuthorizationHeaderValue(Uri uri, string httpMethod, string consumerSecret, string tokenSecret, ICollection<KeyValuePair<string, string>> parameters)
+		{
+			string signatureBase = CreateSignatureBase(uri, httpMethod, parameters, out var newUri);
+
+			parameters.Add(new KeyValuePair<string, string>(OAuthConstants.Signature, CreateHmacSha256Signature(signatureBase, consumerSecret, tokenSecret)));
+
+			return OAuthConstants.HeaderPrefix + " " + parameters
+				.Where(parameter => parameter.Key.StartsWithOrdinal(OAuthConstants.ParameterPrefix) && parameter.Value != null)
+				.Select(p => "{0}=\"{1}\"".FormatInvariant(p.Key, PercentEncode(p.Value)))
+				.Join(",");
+		}
+
 		public static ICollection<KeyValuePair<string, string>> GetHmacSha1Parameters(string consumerToken, INonceCreator nonceCreator, ISystemTime systemTime, Dictionary<string, string>? additionalParameters = null) =>
-			GetHmacShaParameters(consumerToken, nonceCreator.CreateNonce(), DateTimeUtility.ToUnixTimestamp(systemTime.GetUtcNow()).ToInvariantString(), additionalParameters, OAuthSignatureMethods.HmacSha1);
+			GetHmacShaParameters(OAuthSignatureMethods.HmacSha1, consumerToken, nonceCreator.CreateNonce(), DateTimeUtility.ToUnixTimestamp(systemTime.GetUtcNow()).ToInvariantString(), additionalParameters);
 
 		public static ICollection<KeyValuePair<string, string>> GetHmacSha1Parameters(string consumerToken, string nonce, string timeStamp, Dictionary<string, string>? additionalParameters = null) =>
-			GetHmacShaParameters(consumerToken, nonce, timeStamp, additionalParameters, OAuthSignatureMethods.HmacSha1);
+			GetHmacShaParameters(OAuthSignatureMethods.HmacSha1, consumerToken, nonce, timeStamp, additionalParameters);
 
 		public static ICollection<KeyValuePair<string, string>> GetHmacSha256Parameters(string consumerToken, INonceCreator nonceCreator, ISystemTime systemTime, IEnumerable<KeyValuePair<string, string>>? additionalParameters = null) =>
-			GetHmacShaParameters(consumerToken, nonceCreator.CreateNonce(), DateTimeUtility.ToUnixTimestamp(systemTime.GetUtcNow()).ToInvariantString(), additionalParameters, OAuthSignatureMethods.HmacSha256);
+			GetHmacShaParameters(OAuthSignatureMethods.HmacSha256, consumerToken, nonceCreator.CreateNonce(), DateTimeUtility.ToUnixTimestamp(systemTime.GetUtcNow()).ToInvariantString(), additionalParameters);
 
 		public static ICollection<KeyValuePair<string, string>> GetHmacSha256Parameters(string consumerToken, string nonce, string timeStamp, IEnumerable<KeyValuePair<string, string>>? additionalParameters = null) =>
-			GetHmacShaParameters(consumerToken, nonce, timeStamp, additionalParameters, OAuthSignatureMethods.HmacSha256);
+			GetHmacShaParameters(OAuthSignatureMethods.HmacSha256, consumerToken, nonce, timeStamp, additionalParameters);
 
-		private static ICollection<KeyValuePair<string, string>> GetHmacShaParameters(string consumerToken, string nonce, string timeStamp, IEnumerable<KeyValuePair<string, string>>? additionalParameters = null, string? signatureMethod = null) =>
+		private static ICollection<KeyValuePair<string, string>> GetHmacShaParameters(string signatureMethod, string consumerToken, string nonce, string timeStamp, IEnumerable<KeyValuePair<string, string>>? additionalParameters = null) =>
 			new List<KeyValuePair<string, string>>
 			{
 				NewKeyValuePair(OAuthConstants.ConsumerKey, consumerToken),
 				NewKeyValuePair(OAuthConstants.Nonce, nonce),
-				NewKeyValuePair(OAuthConstants.SignatureMethod, signatureMethod ?? OAuthSignatureMethods.HmacSha256),
+				NewKeyValuePair(OAuthConstants.SignatureMethod, signatureMethod),
 				NewKeyValuePair(OAuthConstants.TimeStamp, timeStamp),
 				NewKeyValuePair(OAuthConstants.Version, OAuthConstants.OAuthVersion),
 			}.Union(additionalParameters.EmptyIfNull().Where(kvp => kvp.Value != null)).ToList();
